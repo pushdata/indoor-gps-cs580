e92bdc0b50e33e46e8f93e7f6ec80f5b1983cc18
Sai Prudhvi Chode <prudhvi1105@gmail.com>
2017-04-10 00:50:37 -0400

    Initial Commit!
    Added Permissions to Manifest file
    
    Added Background Color and Event Handlers

---
+++ app/src/main/java/com/wifi/indoorgps/MainActivity.java
@@ -0,0 +1,26 @@
+package com.wifi.indoorgps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView tv = (TextView) findViewById(R.id.textView2);
+                tv.setText(R.string.scan);
+            }
+        });
+    }
+
+
+}
