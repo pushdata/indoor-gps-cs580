078b25bcbca7e7a6d2d38eb9e7e4ed79eca89fc1
Sai Prudhvi Chode <prudhvi1105@gmail.com>
2017-04-16 17:45:51 -0400

    Implemented Data structures to store Fingerprint Data obtained from various points in the map.
    Calculating best RSSI value by averaging the set of RSSI's values

--- app/src/main/java/com/wifi/indoorgps/MainActivity.java
+++ app/src/main/java/com/wifi/indoorgps/MainActivity.java
@@ -1,142 +1,131 @@
 package com.wifi.indoorgps;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.location.LocationManager;
 import android.net.wifi.ScanResult;
-import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.os.Environment;
 import android.os.Handler;
-import android.os.Message;
-import android.util.Log;
-import android.view.KeyEvent;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
-import android.support.v7.app.AppCompatActivity;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.TreeSet;
 
-import static android.app.PendingIntent.FLAG_UPDATE_CURRENT;
+public class MainActivity extends MapActivity {
 
-
-public class MainActivity extends AppCompatActivity {
-    public static String location = "";
-    public static FileOutputStream outputStream;
-    public static PrintWriter file;
-    public static File out;
-    public static WifiManager wifi;
-    public static LocationManager mgr;
-    static String output;
-    TextView tv;
-    private Button startScan;
-    private Button stopScan;
-    private Intent intent;
-    private EditText textBox;
-    private PendingIntent pendingService;
-    private AlarmManager alarmManager;
-    //  this.startScan = (Button) findViewById(R.id.button);
-    //this.stopScan = (Button) findViewById(R.id.button1);
-    private View.OnClickListener scanStartedListener = new View.OnClickListener() {
-        public void onClick(View paramAnonymousView) {
-            MainActivity.this.tv.setText(R.string.scan);
-            onStartService();
-        }
-    };
-    private View.OnClickListener scanStoppedListener = new View.OnClickListener() {
-        public void onClick(View paramAnonymousView) {
-            MainActivity.this.tv.setText(R.string.stopscan);
-
+    public static final int SCAN_DELAY = 1000;
+    public static final int SCAN_INTERVAL = 1000;
+    public static final int MAX_SCAN_THREADS = 2;
+    private static Handler handler = new Handler();
+    private int threadCount = 0;
+    private Runnable refreshArea = new Runnable() {
+        public void run() {
         }
     };
 
-    private void restoreMe(Bundle paramBundle) {
-    }
+    private boolean threadPaused = false;
 
-    private void sendMessageToService(int paramInt) {
-    }
+    private HashMap<String, Integer> readings;
 
-    public void addKeyListener() {
-        this.textBox.setOnKeyListener(new View.OnKeyListener() {
-            public boolean onKey(View paramAnonymousView, int paramAnonymousInt, KeyEvent paramAnonymousKeyEvent) {
-                if ((paramAnonymousKeyEvent.getAction() == 0) && (paramAnonymousInt == 66)) {
-                    Toast.makeText(MainActivity.this, MainActivity.this.textBox.getText(), Toast.LENGTH_SHORT).show();
-                    MainActivity.location = MainActivity.this.textBox.getText().toString();
-                    return true;
-                }
-                return false;
-            }
-        });
-    }
-
-    public void onTimerTick() {
-        output = "";
-        wifi.startScan();
-        Object localObject = wifi.getScanResults();
-        output = output + ((List) localObject).size() + "\n";
-        localObject = ((List) localObject).iterator();
-        for (; ; ) {
-            if (!((Iterator) localObject).hasNext()) {
-                localObject = new SimpleDateFormat("E, dd/MM/yyyy HH:mm:ss").format(Calendar.getInstance().getTime());
-                file.write(output + (String) localObject + " " + MainActivity.location + "\n");
-                file.flush();
-                return;
-            }
-            ScanResult localScanResult = (ScanResult) ((Iterator) localObject).next();
-            output = output + localScanResult.BSSID + " " + localScanResult.SSID + " " + localScanResult.level + "\n";
-        }
-    }
-
-    public void onStartService() {
-        wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        wifi.getConnectionInfo();
-        wifi.getConfiguredNetworks();
-        file = null;
-        try {
-            out = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "fingerprint.txt");
-            if (!out.exists()) {
-                out.createNewFile();
-            }
-            outputStream = new FileOutputStream(Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "fingerprint.txt", true);
-            file = new PrintWriter(outputStream);
-        } catch (FileNotFoundException localFileNotFoundException) {
-            for (; ; ) {
-                Log.d("HHH", "no file!");
-            }
-        } catch (IOException localIOException) {
-            for (; ; ) {
-                Log.d("HHH", "something else");
-            }
-        }
-        onTimerTick();
-    }
-
-    public void onBackPressed() {
-    }
-
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.startScan = ((Button) findViewById(R.id.button));
-        this.stopScan = ((Button) findViewById(R.id.button1));
-        this.tv = ((TextView) findViewById(R.id.textView2));
-        this.textBox = ((EditText) findViewById(R.id.editText1));
-        this.startScan.setOnClickListener(this.scanStartedListener);
-        this.stopScan.setOnClickListener(this.scanStoppedListener);
-        addKeyListener();
-        restoreMe(savedInstanceState);
+        readings = new HashMap<String, Integer>();
+
+        Timer timer = new Timer();
+
+        timer.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                if (threadPaused == false) {
+                    wifi.startScan();
+                }
+            }
+
+        }, SCAN_DELAY, SCAN_INTERVAL);
+    }
+
+    public void onResume() {
+        super.onResume();
+
+        threadPaused = false;
+    }
+
+    public void onPause() {
+        super.onPause();
+        threadPaused = true;
+    }
+
+    @Override
+    public void onReceiveWifiScanResults(final List<ScanResult> results) {
+        FingerprintManager application = (FingerprintManager) getApplication();
+        final ArrayList<Model> fingerprints = application.getFingerprintData(areaSelected);
+
+        if (results.size() > 0 && fingerprints.size() > 0 && threadCount <= MAX_SCAN_THREADS) {
+            Thread t = new Thread() {
+                public void run() {
+                    threadCount++;
+
+                    HashMap<String, Integer> measurements = new HashMap<String, Integer>();
+                    for (ScanResult result : results) {
+                        measurements.put(result.BSSID, result.level);
+                    }
+
+                    TreeSet<String> keys = new TreeSet<String>();
+                    keys.addAll(readings.keySet());
+                    keys.addAll(measurements.keySet());
+
+                    for (String key : keys) {
+                        Integer value = measurements.get(key);
+                        Integer oldValue = readings.get(key);
+                        if (oldValue == null) {
+                            readings.put(key, value);
+                        } else if (value == null) {
+                            readings.remove(key);
+                        } else {
+                            value = (int) (oldValue * 0.4f + value * 0.6f);
+                            readings.put(key, value);
+                        }
+                    }
+
+
+                    Model f = new Model(readings);
+
+                    Model closestMatch = f.getClosestMatch(fingerprints);
+
+                    handler.post(refreshArea);
+
+                    threadCount--;
+                }
+            };
+            t.start();
+        }
+    }
+
+    public void startTrainActivity() {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        menu.add(Menu.NONE, 21, Menu.NONE, "Train Model");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case 21:
+                Toast.makeText(getApplicationContext(), "Traning Activity Called", Toast.LENGTH_LONG).show();
+                startTrainActivity();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
